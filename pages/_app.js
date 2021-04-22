6c62401789583ff7170d439bb17abd5f531fa5d8
László Caputo <L.Caputo27a@gmail.com>
2021-04-22 02:38:41 -0500

    Contact Modal Form & docker filefixed  (#3)
    * update docker-compose
    
    * contact modal / form & fixing dockerfile

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import "../styles/globals.css";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#009696",
+    },
+    secondary: {
+      main: "#2B3D41",
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <MuiThemeProvider theme={theme}>
       {typeof window === "undefined" ? null : <Component {...pageProps} />}
-    </div>
+    </MuiThemeProvider>
   );
 }
 

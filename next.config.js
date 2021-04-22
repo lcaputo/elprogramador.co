3a34f6e27140d37cd7822989819fa411bc2bc4f4
László Caputo <L.Caputo27a@gmail.com>
2021-04-21 21:51:09 -0500

    update next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   distDir: "build",
+  env: {},
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.node = {
+        fs: "empty",
+      };
+    }
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    config.watchOptions = {
+      poll: 3000,
+      aggregateTimeout: 300,
+    };
+    // Important: return the modified config
+    return config;
+  },
 };
